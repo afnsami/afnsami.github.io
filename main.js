a257b4b21a1b2ebe9fb44956953b55bd76f361c7
afnsami <70536934+afnsami@users.noreply.github.com>
2025-12-19 21:35:52 +0600

    Add: projectTitle, Refactor: project Array data order

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const sourceCode = document.getElementById("sourceCode");
 
 
 let projects = [
-    ["images/library_app.png", "https://afnsami.github.io/library-app"],
-    ["images/admin_dashboard.png", "https://afnsami.github.io/admin-dashboard"],
-    ["images/signup_form.png", "https://afnsami.github.io/signup-form"],
-    ["images/calculator.png", "https://afnsami.github.io/calculator"],
-    ["images/pixel_sketch.png", "https://afnsami.github.io/pixel-sketch/"],
-    ["images/rock_paper_scissors.png", "https://afnsami.github.io/rock-paper-scissors/"],
-    ["images/landing_page.png", "https://afnsami.github.io/odin-landing-page/"],
-    ["images/recipes.png", "https://afnsami.github.io/odin-recipes/"],
-    ["images/cursed_farmer.png", "https://github.com/afnsami/CursedFarmer"],
+    ["Book Library", "images/library_app.png", "https://afnsami.github.io/library-app"],
+    ["Admin Dashboard", "images/admin_dashboard.png", "https://afnsami.github.io/admin-dashboard"],
+    ["Sign Up Form", "images/signup_form.png", "https://afnsami.github.io/signup-form"],
+    ["Simple Web Calculator", "images/calculator.png", "https://afnsami.github.io/calculator"],
+    ["Pixel Sketch", "images/pixel_sketch.png", "https://afnsami.github.io/pixel-sketch/"],
+    ["Rock Paper Scissors Game", "images/rock_paper_scissors.png", "https://afnsami.github.io/rock-paper-scissors"],
+    ["Landing Page", "images/landing_page.png", "https://afnsami.github.io/odin-landing-page"],
+    ["Simple Recipes Page", "images/recipes.png", "https://afnsami.github.io/odin-recipes"],
+    ["Cursed Farmer Game", "images/cursed_farmer.png", "https://github.com/afnsami/CursedFarmer"],
 ];
 
 
@@ -22,18 +22,25 @@ let projects = [
 for (let p = 0; p < projects.length; p++) {
     
         let newDiv = document.createElement("div");
+       
         newDiv.className = "projectCard";
         main.appendChild(newDiv);
 
         let imgLink = document.createElement("a");
         let projectImg = document.createElement("img");
 
-        projectImg.src = projects[p][0];
-        imgLink.href = projects[p][1];
+        //Title
+        let projectTitle = document.createElement("h3");
+        projectTitle.className = "projectTitle";
+        projectTitle.innerText = projects[p][0];
+
+        projectImg.src = projects[p][1];
+        imgLink.href = projects[p][2];
         imgLink.target = "_blank";
        
         imgLink.append(projectImg);
         newDiv.append(imgLink);
+        newDiv.append(projectTitle);
 };
 
 
