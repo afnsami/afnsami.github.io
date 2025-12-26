bd51738ff9161c83955190a9f97f71e0f8139a8d
afnsami <70536934+afnsami@users.noreply.github.com>
2025-12-26 16:10:15 +0600

    Add: book info

--- main.js
+++ main.js
@@ -6,15 +6,50 @@ const sourceCode = document.getElementById("sourceCode");
 
 
 let projects = [
-    ["Book Library", "images/library_app.png", "https://afnsami.github.io/library-app"],
-    ["Admin Dashboard", "images/admin_dashboard.png", "https://afnsami.github.io/admin-dashboard"],
-    ["Sign Up Form", "images/signup_form.png", "https://afnsami.github.io/signup-form"],
-    ["Simple Web Calculator", "images/calculator.png", "https://afnsami.github.io/calculator"],
-    ["Pixel Sketch", "images/pixel_sketch.png", "https://afnsami.github.io/pixel-sketch/"],
-    ["Rock Paper Scissors Game", "images/rock_paper_scissors.png", "https://afnsami.github.io/rock-paper-scissors"],
-    ["Landing Page", "images/landing_page.png", "https://afnsami.github.io/odin-landing-page"],
-    ["Simple Recipes Page", "images/recipes.png", "https://afnsami.github.io/odin-recipes"],
-    ["Cursed Farmer Game", "images/cursed_farmer.png", "https://github.com/afnsami/CursedFarmer"],
+    ["Book Library", 
+        "A library to store book lists", 
+        "images/library_app.png", 
+        "https://afnsami.github.io/library-app"],
+
+    ["Admin Dashboard", 
+        "A book library project", 
+        "images/admin_dashboard.png", 
+        "https://afnsami.github.io/admin-dashboard"],
+
+    ["Sign Up Form", 
+        "Stylish sign up form", 
+        "images/signup_form.png", 
+        "https://afnsami.github.io/signup-form"],
+
+    ["Simple Web Calculator", 
+        "A very basic calculator. design inspired by iOS", 
+        "images/calculator.png", 
+        "https://afnsami.github.io/calculator"],
+
+    ["Pixel Sketch Book", 
+        "A pixel art drawing tool", 
+        "images/pixel_sketch.png", 
+        "https://afnsami.github.io/pixel-sketch/"],
+
+    ["Rock Paper Scissors Game", 
+        "Web based game against CPU made with JavaScript", 
+        "images/rock_paper_scissors.png", 
+        "https://afnsami.github.io/rock-paper-scissors"],
+
+    ["Simple Landing Page", 
+        "A landing page for an imaginary clock company", 
+        "images/landing_page.png", 
+        "https://afnsami.github.io/odin-landing-page"],
+
+    ["Simple Recipes Page", 
+        "My first webpage for The Odin Project", 
+        "images/recipes.png", 
+        "https://afnsami.github.io/odin-recipes"],
+
+    ["Cursed Farmer Game", 
+        "A basic Open Source 2d platformer game using Python", 
+        "images/cursed_farmer.png", 
+        "https://github.com/afnsami/CursedFarmer"],
 ];
 
 
@@ -29,18 +64,24 @@ for (let p = 0; p < projects.length; p++) {
         let imgLink = document.createElement("a");
         let projectImg = document.createElement("img");
 
-        //Title
+        //Book title
         let projectTitle = document.createElement("h3");
         projectTitle.className = "projectTitle";
         projectTitle.innerText = projects[p][0];
 
-        projectImg.src = projects[p][1];
-        imgLink.href = projects[p][2];
+        //Book info
+        let projectInfo = document.createElement("p");
+        projectInfo.className = "projectInfo";
+        projectInfo.innerText = projects[p][1];
+
+        projectImg.src = projects[p][2];
+        imgLink.href = projects[p][3];
         imgLink.target = "_blank";
        
         imgLink.append(projectImg);
         newDiv.append(imgLink);
         newDiv.append(projectTitle);
+        newDiv.append(projectInfo);
 };
 
 
